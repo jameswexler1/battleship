a29f7ae5721c495419b6ede36d2a879a187d363c
James Wexler <jamespwexler1@proton.me>
2025-09-04 22:04:03 +0200

    aasdaaaasdasdaasdsddsasdsasddasdasdaasdaakgfjshdksaasdasdasdsasdasasdkjsd

--- copiartudo.py
+++ copiartudo.py
@@ -3,11 +3,15 @@ import os
 # Scan current directory
 root_dir = "."
 output_file = "merged_output.txt"
+skip_dir = os.path.join(root_dir, "public")  # directory to ignore
 
 with open(output_file, "wb") as out_f:  # open in binary mode for all file types
     for dirpath, dirnames, filenames in os.walk(root_dir):
         # Skip hidden directories
         dirnames[:] = [d for d in dirnames if not d.startswith(".")]
+
+        # Skip 'public' directory
+        dirnames[:] = [d for d in dirnames if os.path.join(dirpath, d) != skip_dir]
 
         for filename in filenames:
             # Skip hidden files and the output file itself
